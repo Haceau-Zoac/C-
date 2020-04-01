821438452f260dd4964d5dae13bb64a197f07029
Haceau-Zoac <61569726+Haceau-Zoac@users.noreply.github.com>
2020-04-01 18:00:55 +0800

    Add files via upload

---
+++ goto语句/goto.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+# include <stdlib.h>
+using namespace std;
+
+int main(void)
+{
+    //goto”Ôæ‰
+
+    cout << "1.xxxx" << endl;
+
+    cout << "2.xxxx" << endl;
+
+    goto FLAG;
+
+    cout << "3.xxxx" << endl;
+
+    cout << "4.xxxx" << endl;
+
+    FLAG :
+
+    cout << "5.xxxx" << endl;
+
+    system("pause");
+
+    return 0;
+}
---
+++ 注释/02 注释.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+# include <stdlib.h>
+using namespace std;
+
+//1、单行注释
+
+//2、多行注释
+
+/*
+    main是一个程序的入口
+    每个程序都必须有一个函数
+    有且仅有一个
+
+*/
+
+int main()
+{
+    //12行代码韩式就是在屏幕中输出Hello world
+    cout << "Hello world" << endl;
+
+    system("pause");
+
+    return 0;
+}
